2d8950e534def3340345ea165eac0cb2dce3812c
R. da Silva Almeida <r.da.silva.almeida@avanade.com>
2020-02-04 09:00:17 -0300

    deploy

---
+++ build/precache-manifest.6a1c2cc9d7f7f6ecd98fc1891a5c699b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7084a4c1db90a0de0f6f6f37453c6a1",
+    "url": "/portifolioGitHub/index.html"
+  },
+  {
+    "revision": "badaf91844fce95383ee",
+    "url": "/portifolioGitHub/static/css/main.f693ba2c.chunk.css"
+  },
+  {
+    "revision": "781d4ba5332b3de08ee3",
+    "url": "/portifolioGitHub/static/js/2.83eb9d53.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portifolioGitHub/static/js/2.83eb9d53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "badaf91844fce95383ee",
+    "url": "/portifolioGitHub/static/js/main.4a13afaf.chunk.js"
+  },
+  {
+    "revision": "a977fd2d54b4ba0b0870",
+    "url": "/portifolioGitHub/static/js/runtime-main.b3405d85.js"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/portifolioGitHub/static/media/Octocat.0ba2aa20.png"
+  },
+  {
+    "revision": "5c3ef0fbab365f033062c1b7583452de",
+    "url": "/portifolioGitHub/static/media/github-logo-icon.5c3ef0fb.png"
+  }
+]);
